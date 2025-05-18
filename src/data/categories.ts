3ffd2163b4a76018046eb989f735092fdb7d449f
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-05-18 17:03:47 +0530

    update category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -8,28 +8,27 @@ export const categories: Category[] = [
     subcategories: [
       { id: 'aluminium', name: 'Aluminium', imageUrl: 'https://images.pexels.com/photos/5691544/pexels-photo-5691544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
       { id: 'blocks', name: 'Blocks', imageUrl: 'https://images.pexels.com/photos/3334360/pexels-photo-3334360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'cement&Concrete', name: 'Cement & Concrete', imageUrl: 'https://images.pexels.com/photos/2092078/pexels-photo-2092078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'cement-concrete', name: 'Cement & Concrete', imageUrl: 'https://images.pexels.com/photos/2092078/pexels-photo-2092078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
       { id: 'chemical', name: 'Chemical', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'insulation', name: 'Insulation', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'paint', name: 'Paint', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'panels-partitions', name: 'Panels & Partitions', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'sealant-adhesive', name: 'Sealant & Adhesive', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'steel-items', name: 'Steel Items', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'tiles', name: 'Tiles', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'timber', name: 'Timber', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+      { id: 'waterproofing', name: 'Waterproofing', imageUrl: 'https://images.pexels.com/photos/4513940/pexels-photo-4513940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'}
     ],
   },
   {
-    id: 'kitchen-&-Bathroom',
-    name: 'Kitchen & Bathroom',
-    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAeA5S87d-Z1Zu_0uYOJ7_w0N4PRKlHucUnJZAkgMYBrhu19udgP_W_8574coyS7Mom5g&usqp=CAU',
-    subcategories: [
-      { id: 'lighting', name: 'Lighting', imageUrl: 'https://images.pexels.com/photos/577514/pexels-photo-577514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'wiring', name: 'Wiring', imageUrl: 'https://images.pexels.com/photos/1109541/pexels-photo-1109541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'switches', name: 'Switches & Sockets', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-    ],
-  },
-  {
-    id: 'doors-&-Windows',
+    id: 'doors-windows',
     name: 'Doors & Windows',
     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
     subcategories: [
-      { id: 'pipes', name: 'Pipes & Fittings', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'pumps', name: 'Pumps', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'faucets', name: 'Faucets & Fixtures', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'locks-handles', name: 'Locks & Handles', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'door-window-accessories', name: 'Door Window Accessories', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'door-sashes', name: 'Door Sashes', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'fire-rated', name: 'Fire Rated', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
     ],
   },
   {
@@ -37,9 +36,113 @@ export const categories: Category[] = [
     name: 'Electrical',
     imageUrl: 'https://media.istockphoto.com/id/1130957259/photo/composition-with-tools-and-industrial-material-with-industrial-ship-background.jpg?s=612x612&w=0&k=20&c=XDZNptsfq3TASTKWGsmnDajREC820JuDQf9IyRanCsg=',
     subcategories: [
-      { id: 'paint', name: 'Paint', imageUrl: 'https://images.pexels.com/photos/5582597/pexels-photo-5582597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'tiles', name: 'Tiles', imageUrl: 'https://images.pexels.com/photos/5260554/pexels-photo-5260554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
-      { id: 'flooring', name: 'Flooring', imageUrl: 'https://images.pexels.com/photos/1603884/pexels-photo-1603884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'wires-cables', name: 'Wires & Cables', imageUrl: 'https://images.pexels.com/photos/5582597/pexels-photo-5582597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'electrical-switches-sockets', name: 'Electrical Switches & Sockets', imageUrl: 'https://images.pexels.com/photos/5260554/pexels-photo-5260554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'distribution-boards', name: 'Distribution Boards & Circuit Protection', imageUrl: 'https://images.pexels.com/photos/1603884/pexels-photo-1603884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'lighting', name: 'Lighting', imageUrl: 'https://images.pexels.com/photos/1603884/pexels-photo-1603884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'electrical-accessories', name: 'Electrical Accessories', imageUrl: 'https://images.pexels.com/photos/1603884/pexels-photo-1603884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'smart-low-voltage-systems', name: 'Smart and Low Voltage Systems', imageUrl: 'https://images.pexels.com/photos/1603884/pexels-photo-1603884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'home-appliances', name: 'Home Appliances', imageUrl: 'https://images.pexels.com/photos/1603884/pexels-photo-1603884.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'flooring',
+    name: 'Flooring',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
+    subcategories: [
+      { id: 'carpets', name: 'Carpets', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'epoxy-flooring', name: 'Epoxy Flooring', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'industrial-flooring', name: 'Industrial Flooring', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'outdoor-decking', name: 'Outdoor Decking', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'rendered-flooring', name: 'Rendered Flooring', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'titanium-flooring', name: 'Titanium Flooring', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'vinyl-flooring', name: 'Vinyl Flooring', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'wood-flooring', name: 'Wood Flooring', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'furniture',
+    name: 'Furniture',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
+    subcategories: [
+      { id: 'living-room', name: 'Living Room', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'bedroom', name: 'Bedroom', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'dining-room', name: 'Dining Room', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'home-office', name: 'Home Office', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'office-furniture', name: 'Office Furniture', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'storage-furniture', name: 'Storage Furniture', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'hardware',
+    name: 'Hardware',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
+    subcategories: [
+      { id: 'nails', name: 'Nails', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'nuts-bolts', name: 'Nuts & Bolts', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'plugs-anchors', name: 'Plugs & Anchors', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'screws', name: 'Screws', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'kitchen-bathroom',
+    name: 'Kitchen & Bathroom',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAeA5S87d-Z1Zu_0uYOJ7_w0N4PRKlHucUnJZAkgMYBrhu19udgP_W_8574coyS7Mom5g&usqp=CAU',
+    subcategories: [
+      { id: 'sanitary-ware', name: 'Sanitary Ware', imageUrl: 'https://images.pexels.com/photos/577514/pexels-photo-577514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'faucets-taps', name: 'Faucets and Taps', imageUrl: 'https://images.pexels.com/photos/1109541/pexels-photo-1109541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'sinks', name: 'Sinks', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'bathroom-accessories', name: 'Bathroom Accessories', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'showers-bathtubs', name: 'Showers and Bathtubs', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'cabinetry-vanities', name: 'Cabinetry & Vanities', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'countertops', name: 'Countertops', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'ventilation-exhaust', name: 'Ventilation & Exhaust Systems', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'water-heating', name: 'Water Heating Systems', imageUrl: 'https://images.pexels.com/photos/6444366/pexels-photo-6444366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'plumbing',
+    name: 'Plumbing',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
+    subcategories: [
+      { id: 'conduits-casings', name: 'Conduits & Casings', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'fire-suppression', name: 'Fire Suppression Equipment', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'plumbing-valves', name: 'Plumbing Valves & Accessories', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'pipe-fittings', name: 'Pipe Fittings', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'ppr-pipes', name: 'PPR Pipes & Fittings', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'water-pumps', name: 'Water Pumps', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'pvc-pipes', name: 'PVC & Other Pipes', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'septic-sewage', name: 'Septic & Sewage', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'water-filtration', name: 'Water Filtration', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'water-tanks', name: 'Water Tanks & Storage', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'roofing-ceiling',
+    name: 'Roofing & Ceiling',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
+    subcategories: [
+      { id: 'roofing-accessories', name: 'Roofing Accessories', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'roofing-sheets', name: 'Roofing Sheets', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'roof-tiles', name: 'Roof Tiles', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'ceiling-tiles', name: 'Ceiling Tiles', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
+    ],
+  },
+  {
+    id: 'tools-equipment',
+    name: 'Tools & Equipment',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi9ymN1fk9OzMZbOCHNKG4JMtDq4zlV8vhJw&s',
+    subcategories: [
+      { id: 'automotive', name: 'Automotive', imageUrl: 'https://images.pexels.com/photos/4491014/pexels-photo-4491014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'hand-tools', name: 'Hand Tools', imageUrl: 'https://images.pexels.com/photos/4491881/pexels-photo-4491881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'masonry-tools', name: 'Masonry Tools', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'measuring-equipment', name: 'Measuring Equipment', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'power-tools', name: 'Power Tools', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'safety-equipment', name: 'Safety Equipment', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'scaffolding', name: 'Scaffolding', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'welding-equipment', name: 'Welding Equipment', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'lifting-equipment', name: 'Lifting Equipment', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' },
+      { id: 'landscape-external', name: 'Landscape & External Works', imageUrl: 'https://images.pexels.com/photos/369598/pexels-photo-369598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' }
     ],
   },
 ];
