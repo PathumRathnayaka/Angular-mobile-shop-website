bf4e7818e1193852cb706b409fbe00cfdb10b67e
Pathum Rathnayaka <thilinapathumrathnayaka@gmail.com>
2025-10-22 08:28:50 +0530

    fixed eror

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
-// src/app/app.ts
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { PhoneListComponent } from './components/phone-list/phone-list.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavbarComponent, BannerComponent, PhoneListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('mobile-shop-landing');
+}
